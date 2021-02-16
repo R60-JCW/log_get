45d7bc96ecb1957b7c0725c4cc266ca7f6c91aca
Kimura <kimura@default.com>
2021-02-15 19:18:31 -0800

    build用のシェル作成（動作未確認)

---
+++ health_check/build.sh
@@ -0,0 +1,8 @@
+# git config
+git config --global user.name "Kimura"
+git config --global user.email kimura@default.com
+
+# add remote repository for upload
+git clone https://github.com/tk-yahoo/upload.git facts
+git clone https://github.com/tk-yahoo/upload-test.git tmp-facts
+
