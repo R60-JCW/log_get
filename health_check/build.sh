d75b49bd87d2e1c22300e581fd4a3c3e8af46357
Kimura <kimura@default.com>
2021-05-18 01:55:30 +0000

    dddd

--- health_check/build.sh
+++ health_check/build.sh
@@ -3,6 +3,6 @@ git config --global user.name "Kimura"
 git config --global user.email kimura@default.com
 
 # add remote repository for upload
-git clone https://github.com/tk-yahoo/upload.git facts
-git clone https://github.com/tk-yahoo/upload-test.git tmp-facts
+#git clone https://github.com/tk-yahoo/upload.git facts
+#git clone https://github.com/tk-yahoo/upload-test.git tmp-facts
 
