2fdc09d10c5f2355d478ab727e5cee23361139bc
hogehoge <hogehoge@example.com>
2021-06-25 14:07:12 -0700

    [add file]shell for setup ansible

---
+++ health_check/setup.sh
@@ -0,0 +1,9 @@
+sudo yum install -y python36 python36-pip python36-devel
+sudo pip-3.6 install --upgrade pip
+pip unistall ansible
+pip3 install ansible
+pip3 install ntc-templates
+pip install --upgrade pip
+ansible --version
+git config --global user.email "hogehoge@example.com"
+git config --global user.name "hogehoge"
